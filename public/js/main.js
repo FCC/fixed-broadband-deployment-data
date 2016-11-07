70f3823d990b9842d1a97b502234bfc9c912a8ff
bizui <bizuilize@gmail.com>
2016-11-07 12:30:00 -0500

    add map modules and Browserify

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    var Map = require('./modules/map.js');
+    var MapSearch = require('./modules/map-search.js');
+
+    Map.init();
+    MapSearch.init();
+}());
