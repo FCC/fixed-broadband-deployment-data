e2253c94568f5729fa4f554810cfef7098e4247c
bizui <bizuilize@gmail.com>
2016-11-16 14:20:16 -0500

    fix county layer display

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 (function() {
     'use strict';
 
-    var Map = require('./modules/map.js');
+    var BPRMap = require('./modules/map.js');
     var MapSearch = require('./modules/map-search.js');
 
-    Map.init();
+    BPRMap.init();
     MapSearch.init();
 }());
