acb7121f7b2043ae8f84ff0625a367729354ba9a
bizui <bizuilize@gmail.com>
2016-11-17 18:19:33 -0500

    add chart placeholders

---
+++ public/js/modules/chart-fixed.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var chartData = {
+    labels: ["All", "Urban", "Rural", "Tribal"],
+    datasets: [{
+        label: 'Both',
+        data: [727, 589, 537, 543],
+        backgroundColor: 'rgba(255, 99, 132, 0.4)'
+    }, {
+        label: 'Fixed',
+        data: [238, 553, 746, 884],
+        backgroundColor: 'rgba(54, 162, 235, 0.4)'
+    }, {
+        label: 'Mobile',
+        data: [1238, 553, 746, 884],
+        backgroundColor: 'rgba(255, 206, 86, 0.4)'
+    }, {
+        label: 'None',
+        data: [584, 789, 254, 654],
+        backgroundColor: 'rgba(54, 162, 235, 0.4)'
+    }]
+};
+
+var chartFixed = {
+    create: function(data) {
+        var ctxFixed = $('#chartFixed');
+
+        if ($('#chartFixed').length > 0) {
+            var fixedChart = new Chart(ctxFixed, {
+                type: 'bar',
+                data: chartData,
+                options: {
+                    scales: {
+                        xAxes: [{
+                            stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true
+                        }]
+                    }
+                }
+            });
+        }
+    }
+};
+
+module.exports = chartFixed;
