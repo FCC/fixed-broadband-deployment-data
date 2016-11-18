1df72f155f455fa98299c4ee3b3dbb9c3260524e
bizui <bizuilize@gmail.com>
2016-11-18 17:14:36 -0500

    add legend colors

--- js/modules/layers-tech.js
+++ js/modules/layers-tech.js
@@ -7,35 +7,40 @@ layersTech['FTTP'] = {
     format: 'image/png',
     transparent: true,
     layers: 'bpr_dec2016_tech_fiber',
-    styles: 'bpr_dec2016_tech'
+    styles: 'bpr_dec2016_tech',
+    color: '#ffffcc'
 };
 
 layersTech['Cable modem'] = {
     format: 'image/png',
     transparent: true,
     layers: 'bpr_dec2016_tech_cable',
-    styles: 'bpr_dec2016_tech'
+    styles: 'bpr_dec2016_tech',
+    color: '#6e016b'
 };
 
 layersTech['DSL (inc. FTTN), other copper'] = {
     format: 'image/png',
     transparent: true,
     layers: 'bpr_dec2016_tech_adsl',
-    styles: 'bpr_dec2016_tech'
+    styles: 'bpr_dec2016_tech',
+    color: '#6e016b'
 };
 
 layersTech['Fixed wireless'] = {
     format: 'image/png',
     transparent: true,
     layers: 'bpr_dec2016_tech_fw',
-    styles: 'bpr_dec2016_tech'
+    styles: 'bpr_dec2016_tech',
+    color: '#6e016b'
 };
 
 layersTech['Other'] = {
     format: 'image/png',
     transparent: true,
     layers: 'bpr_dec2016_tech_other',
-    styles: 'bpr_dec2016_tech'
+    styles: 'bpr_dec2016_tech',
+    color: '#6e016b'
 };
 
 module.exports = layersTech;
