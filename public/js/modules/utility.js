4515cf06ebaeff89bea207cf1987edbb4843f664
bizui <bizuilize@gmail.com>
2016-11-21 12:58:50 -0500

    fix function expression

--- public/js/modules/utility.js
+++ public/js/modules/utility.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var utility = {
-    isNull(fieldName) {
+    isNull: function(fieldName) {
         return fieldName === null;
     },
-    formatComma(num) {
+    formatComma: function(num) {
         var parts = num.toString().split('.');
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         return parts.join('.');
     },
-    formatPercent(num) {
+    formatPercent: function(num) {
         return (num * 100).toFixed(2) + '%';
     }
 };
