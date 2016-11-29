bf85d03a38a6b117b9e7cac6036ad08185794604
bizui <bizuilize@gmail.com>
2016-11-29 15:45:05 -0500

    add Tribal data; update URL's for Urban and Rural API

--- public/js/modules/chart-fixed.js
+++ public/js/modules/chart-fixed.js
@@ -2,10 +2,8 @@
 
 var chartFixed = {
     init: function(county_fips) {
-        // var ctxFixed, fixedChart;
-
         chartFixed.data = {
-            labels: ['All', 'Urban', 'Rural'],
+            labels: ['All', 'Urban', 'Rural', 'Tribal'],
             datasets: [{
                 label: 'Fixed',
                 data: [],
@@ -16,6 +14,11 @@ var chartFixed = {
                 backgroundColor: '#6CBCD5'
             }]
         };
+
+        //show County Fixed chart if it exists on the page
+        if ($('#chartFixed').length === 0) {
+            return;
+        }
 
         //if county FIPS is the same don't regenerate chart
         if (county_fips === chartFixed.FIPS) {
@@ -34,35 +37,62 @@ var chartFixed = {
             url: allCntyURL,
             success: function(data) {
                 chartFixed.update(data);
-                chartFixed.getUrbanData();
+                chartFixed.getURData();
+                
             }
         });
     },
-    getUrbanData: function() {
-        var urbanURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fcc:bpr_dec2016_county_urban_fnf&maxFeatures=100&outputFormat=application/json&cql_filter=county_fips=%27' + chartFixed.FIPS + '%27';
+    getURData: function() {
+        var urURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bpr_dec2016_county_ur_fnf&maxFeatures=100&outputFormat=application/json&cql_filter=county_fips=%27' + chartFixed.FIPS + '%27';
 
         $.ajax({
             type: 'GET',
-            url: urbanURL,
+            url: urURL,
             success: function(data) {
-                chartFixed.update(data);
-                chartFixed.getRuralData();
-            }
+                chartFixed.processURData(data);
+                chartFixed.getTribalData();
+             }
         });
     },
-    getRuralData: function() {
-        var ruralURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fcc:bpr_dec2016_county_rural_fnf&maxFeatures=100&outputFormat=application/json&cql_filter=county_fips=%27' + chartFixed.FIPS + '%27';
+    getTribalData: function() {
+        var tribalURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bpr_dec2016_county_tribal_fnf&maxFeatures=100&outputFormat=application/json&cql_filter=county_fips=%27' + chartFixed.FIPS + '%27';
 
         $.ajax({
             type: 'GET',
-            url: ruralURL,
-            success: function(data) {
+            url: tribalURL,
+            success: function(data) { 
                 chartFixed.update(data);
                 chartFixed.display();
-            }
+             }
         });
     },
+    processURData: function(data) {
+        var i = 0;
+        var dataLen = data.features.length;
+
+        var urbanData = {};
+        var ruralData = {};
+
+        urbanData.features = [];       
+        ruralData.features = [];
+
+        for (i; i < dataLen; i++) {
+            switch (data.features[i].properties.urban_rural) {
+                case 'U':
+                    urbanData.features.push(data.features[i]);
+                    break;
+                case 'R':
+                    ruralData.features.push(data.features[i]);
+                    break;
+            }
+        }
+
+        chartFixed.update(urbanData);
+        chartFixed.update(ruralData);
+        
+    },    
     update: function(data) {
+        var i = 0;
         var fixedData = chartFixed.data.datasets[0].data;
         var noFixedData = chartFixed.data.datasets[1].data;
 
@@ -73,27 +103,26 @@ var chartFixed = {
             return;
         }
 
-        for (var i = 0; i < data.features.length; i++) {
+        for (i = 0; i < data.features.length; i++) {
             switch (data.features[i].properties.has_fixed) {
                 case 0:
-                    noFixedData.push(data.features[i].properties.type_pop_pct.toFixed(2));
+                    noFixedData.push(data.features[i].properties.pop_pct.toFixed(2));
 
-                    if (data.features[i].properties.type_pop_pct === 100) {
+                    if (data.features[i].properties.pop_pct === 100) {
                         fixedData.push(0);
                     }
 
                     break;
                 case 1:
-                    fixedData.push(data.features[i].properties.type_pop_pct.toFixed(2));
+                    fixedData.push(data.features[i].properties.pop_pct.toFixed(2));
 
-                    if (data.features[i].properties.type_pop_pct === 100) {
+                    if (data.features[i].properties.pop_pct === 100) {
                         noFixedData.push(0);
                     }
 
                     break;
             }
-        }
-
+        }        
     },
     display: function() {
         var ctxFixed;
