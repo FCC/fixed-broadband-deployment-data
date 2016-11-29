c47435502fe3d68c81e9c5c2f7159a15e8454d22
bizui <bizuilize@gmail.com>
2016-11-29 15:47:16 -0500

    add Tribal data

--- js/modules/chart-fixedNationwide.js
+++ js/modules/chart-fixedNationwide.js
@@ -1,68 +1,63 @@
 'use strict';
 
 var chartNWFixed = {
+    data: {
+        labels: ['All', 'Urban', 'Rural', 'Tribal'],
+        datasets: [{
+            label: 'Fixed',
+            data: [],
+            backgroundColor: '#FFE773'
+        }, {
+            label: 'No Fixed',
+            data: [],
+            backgroundColor: '#6CBCD5'
+        }]
+    },
     init: function() {
-        // var ctxFixed, fixedChart;
-
-        chartNWFixed.data = {
-            labels: ['All', 'Urban', 'Rural'],
-            datasets: [{
-                label: 'Fixed',
-                data: [],
-                backgroundColor: '#FFE773'
-            }, {
-                label: 'No Fixed',
-                data: [],
-                backgroundColor: '#6CBCD5'
-            }]
-        };
-
-        //if county FIPS is the same don't regenerate chart
-        /*if (county_fips === chartNWFixed.FIPS) {
+        //show Nationwide chart if it exists on the page
+        if ($('#chartNWFixed').length === 0) {
             return;
-        } else {
-            chartNWFixed.FIPS = county_fips;
-        }*/
+        }
 
         chartNWFixed.getNWData();
-    },    
-    getNWData: function() { 
+    },
+    getNWData: function() {
         var nwURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fcc:bpr_dec2016_nw_fnf&maxFeatures=100&outputFormat=application/json';
 
         $.ajax({
             type: 'GET',
             url: nwURL,
-            success: function(data) {
+            success: function(data) { 
                 chartNWFixed.update(data);
                 chartNWFixed.getUrbanData();
             }
         });
     },
     getUrbanData: function() {
-        var urbanURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fcc:bpr_dec2016_nw_urban_rural_fnf&maxFeatures=100&outputFormat=application/json';
+        var urbanURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bpr_dec2016_nw_ur_fnf&maxFeatures=100&outputFormat=application/json';
 
         $.ajax({
             type: 'GET',
             url: urbanURL,
             success: function(data) {
                 chartNWFixed.update(data);
-                chartNWFixed.display();
+                chartNWFixed.getTribal(data);
             }
         });
     },
-    /*getRuralData: function() {
-        var ruralURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=fcc:bpr_dec2016_county_rural_fnf&maxFeatures=100&outputFormat=application/json&cql_filter=county_fips=' + chartNWFixed.FIPS;
+    getTribal: function() {
+        var tribalURL = '/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=bpr_dec2016_nw_tribal_fnf&maxFeatures=100&outputFormat=application/json';
 
         $.ajax({
             type: 'GET',
-            url: ruralURL,
+            url: tribalURL,
             success: function(data) {
                 chartNWFixed.update(data);
                 chartNWFixed.display();
             }
         });
-    },*/
-    update: function(data) { 
+    },
+    update: function(data) {
         var fixedData = chartNWFixed.data.datasets[0].data;
         var noFixedData = chartNWFixed.data.datasets[1].data;
 
@@ -73,31 +68,31 @@ var chartNWFixed = {
             return;
         }
 
-        for (var i = 0; i < data.features.length; i++) {
+        for (var i = 0; i < data.features.length; i++) { 
             switch (data.features[i].properties.has_fixed) {
                 case 0:
-                    noFixedData.push(data.features[i].properties.type_pop_pct.toFixed(2));
-                    
+                    noFixedData.push(data.features[i].properties.pop_pct.toFixed(2));
+
                     if (data.features[i].properties.type_pop_pct === 100) {
                         fixedData.push(0);
                     }
 
                     break;
-                case 1:
-                    fixedData.push(data.features[i].properties.type_pop_pct.toFixed(2));
-                    
+                case 1:                
+                    fixedData.push(data.features[i].properties.pop_pct.toFixed(2));
+
                     if (data.features[i].properties.type_pop_pct === 100) {
                         noFixedData.push(0);
                     }
 
                     break;
             }
-        }       
-        
+        }
+
     },
     display: function() {
-        var  ctxNWFixed = $('#chartNWFixed');
-        
+        var ctxNWFixed = $('#chartNWFixed');
+
         //create new chart
         chartNWFixed.chart = new Chart(ctxNWFixed, {
             type: 'bar',
